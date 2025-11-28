64227834b26fe01efc89bc2e5060e1107ce05e2b
Raphael Willian <raphaelwillian.bdp44@gmail.com>
2025-11-28 16:15:24 -0300

    Initial commit: Add initial project files

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Bem-vindo à API de E-commerce! em Node.js + Express");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
