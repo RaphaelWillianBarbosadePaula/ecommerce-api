fc8040c794d3de9cfe89650a78ef9528010318e1
Raphael Willian <raphaelwillian.bdp44@gmail.com>
2025-12-11 23:34:10 -0300

    add: initial http methods study on express

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,59 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
     res.send("Bem-vindo à API de E-commerce! em Node.js + Express - Teste TSC Watch");
+});
+
+type User = {
+    id: number; 
+    nome: string;
+    email: string;
+}
+
+let id = 0;
+let usuarios: User[] = [];
+
+app.get("/users", (req: Request, res: Response) => {
+    res.send(usuarios);
+});
+
+app.get("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let user = usuarios.find(user => user.id === userId);
+    res.send(user);
+})
+
+app.post("/users", (req: Request, res: Response) => {
+    let user = req.body;
+    user.id = ++id;
+    usuarios.push(user);
+    res.send({
+        message: "Usuário criado com sucesso!"
+    })
+});
+
+app.put("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let user = req.body;
+    let indexOf = usuarios.findIndex((user: User) => user.id === userId);
+    usuarios[indexOf].nome = user.nome;
+    usuarios[indexOf].email = user.email;
+    res.send({
+        message: "Usuário alterado com sucesso!",
+        user
+    });
+});
+
+app.delete("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let indexOf = usuarios.findIndex((user: User) => user.id === userId);
+    usuarios.splice(indexOf, 1);
+    res.send({
+        message: "Usuário excluído com sucesso!"
+    })
 });
 
 app.listen(3000, () => {
